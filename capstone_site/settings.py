a4b8b72b17ce3c5da8a248ed4327566f74089516
Mbeko Sifolo <mbeko007@gmail.com>
2024-06-06 04:10:05 +0200

    dockerised site

--- capstone_site/settings.py
+++ capstone_site/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-omgocjoa%q3*sg4og%s0c$15%@2&%+f6!4h+23lmgvbsohbi2m'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
